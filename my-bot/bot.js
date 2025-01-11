8711d1515008806448cdd1e8d8398c3bf7fece16
aakifxon <aatakul28@gmail.com>
2025-01-11 18:45:55 +0300

    tugatildi

--- my-bot/bot.js
+++ my-bot/bot.js
@@ -1,16 +1,16 @@
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
 
-const bot = new Telegraf('7656292869:AAE7bonRy47PLd-YyNGLqDNFHKdKAEMKMUs'); // Sizning bot tokeningiz
+const bot = new Telegraf('7656292869:AAE7bonRy47PLd-YyNGLqDNFHKdKAEMKMUs'); 
 
 const getData = async () => {
   try {
     const response = await axios.get('http://localhost:3001/data');
     const data = response.data;
     if (data) {
-      const messagesArray = data.map(message => message.text); // `name` o'rniga `text` ustunidan ma'lumot olish
+      const messagesArray = data.map(message => message.text); 
       const randomMessage = messagesArray[Math.floor(Math.random() * messagesArray.length)];
-      bot.telegram.sendMessage('@botnitekshiruviuchun', randomMessage); // Kanal nomini o'zingiznikiga moslang
+      bot.telegram.sendMessage('@botnitekshiruviuchun', randomMessage); 
     }
   } catch (error) {
     console.error('Xato:', error);
--- my-bot/database.js
+++ my-bot/database.js
@@ -2,7 +2,6 @@ const sqlite3 = require('sqlite3').verbose();
 const { initializeApp } = require('firebase/app');
 const { getDatabase, ref, onValue } = require('firebase/database');
 
-// Firebase konfiguratsiyasi
 const firebaseConfig = {
   apiKey: "AIzaSyBqjQ2SosKX_OFRRiybn0y5AXOc02Bq2Gg",
   authDomain: "xabar-bot-abba1.firebaseapp.com",
@@ -14,31 +13,30 @@ const firebaseConfig = {
   measurementId: "G-17Y303378H"
 };
 
-// Firebase ilovasini ishga tushirish
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// SQLite ma'lumotlar bazasi faylini yaratish yoki unga ulanish
-const db = new sqlite3.Database('mydatabase.db'); // Faylda saqlash uchun
 
-// Jadval yaratish
+const db = new sqlite3.Database('mydatabase.db');
+
 db.serialize(() => {
-  // Eski jadvalni o'chirish (agar mavjud bo'lsa)
+
   db.run("DROP TABLE IF EXISTS mytable");
   
-  // Yangi jadval yaratish
+
   db.run("CREATE TABLE mytable (id INTEGER PRIMARY KEY, text TEXT, timestamp TEXT)");
 
-  // Firebase Realtime Database dan ma'lumotlarni olish va SQLite ga kiritish
+
   const messagesRef = ref(database, 'messages');
   onValue(messagesRef, (snapshot) => {
     const data = snapshot.val();
-    console.log('Firebase ma\'lumotlari:', data); // Konsolga chiqarish
+    console.log('Firebase ma\'lumotlari:', data); 
 
     if (data) {
       const stmt = db.prepare("INSERT INTO mytable (text, timestamp) VALUES (?, ?)");
       Object.values(data).forEach(message => {
-        console.log('Kiritilayotgan ma\'lumot:', message); // Ma'lumotni konsolga chiqarish
+        console.log('Kiritilayotgan ma\'lumot:', message); 
         stmt.run(message.text, message.timestamp);
       });
       stmt.finalize();
